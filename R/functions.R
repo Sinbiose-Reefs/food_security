4f2434eff03e8c40784694f9b22ee1a48a3b3f29
andreluza <luza.andre@gmail.com>
2022-11-21 22:47:41 -0300

    names changed

--- R/functions.R
+++ R/functions.R
@@ -18,3 +18,20 @@ poison_smooth <- function(...) {
               method.args = list(family = "negbin(1)"),...)
   # geom_smooth(method = "glm", method.args = list(family = "poisson"), ...) # glm option
 }
+
+
+# FUnction
+# point within polygon
+st_centroid_within_poly <- function (poly) {
+  
+  # check if centroid is in polygon
+  centroid <- poly %>% st_centroid() 
+  in_poly <- st_within(centroid, poly, sparse = F)[[1]] 
+  
+  # if it is, return that centroid
+  if (in_poly) return(centroid) 
+  
+  # if not, calculate a point on the surface and return that
+  centroid_in_poly <- st_point_on_surface(poly) 
+  return(centroid_in_poly)
+}
