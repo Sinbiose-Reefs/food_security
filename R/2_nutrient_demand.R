d10982cc26904a717fc60efc33721835b54d07bf
andreluza <luza.andre@gmail.com>
2022-11-30 22:50:57 -0300

    circular plot and map

--- R/2_nutrient_demand.R
+++ R/2_nutrient_demand.R
@@ -91,6 +91,113 @@ foodType_ind %>%
 dev.off()
 
 
+# circular plot
+# https://r-graph-gallery.com/297-circular-barplot-with-groups.html
+
+# aggregate
+dat_circular <- foodType_ind %>%
+  select (-`(all)`) %>%
+  filter (is.na(Beef) !=T) %>%
+  group_by(income_cat,region) %>%
+  summarise(across (Beef:Seafood,~ mean(.x, na.rm = TRUE))) %>% 
+  gather ("food_type", "proportion", -income_cat,-region) %>%
+  mutate(food_type = factor(food_type, levels = c("Beef", 
+                                                  "Beef&other",
+                                                  "Game",
+                                                  "Goat",
+                                                  "Pork",
+                                                  "Poultry",
+                                                  "Freshwater fish",
+                                                  "Imported fish",
+                                                  "Seafood")),
+         proportion = proportion*100)
+
+
+# library
+library(tidyverse)
+library(viridis)
+
+
+
+# Set a number of 'empty bar' to add at the end of each group
+empty_bar <- 1
+nObsType <- nlevels(as.factor(dat_circular$food_type))
+to_add <- data.frame( matrix(NA, empty_bar*nlevels(as.factor(dat_circular$region))*nObsType, ncol(dat_circular)) )
+colnames(to_add) <- colnames(dat_circular)
+to_add$region <- rep(levels(as.factor(dat_circular$region)), each=empty_bar*nObsType )
+dat_circular <- rbind(dat_circular, to_add)
+dat_circular <- dat_circular %>% arrange(region, income_cat)
+dat_circular$id <- rep( seq(1, nrow(dat_circular)/nObsType) , each=nObsType)
+
+# Get the name and the y position of each label
+label_data <- dat_circular %>% group_by(id, income_cat) %>% 
+  summarize(tot=sum(proportion,na.rm=T))
+number_of_bar <- nrow(label_data)
+angle <- 90 - 360 * (label_data$id-0.5) /number_of_bar     # I substracted 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
+label_data$hjust <- ifelse( angle < -90, 1, 0)
+label_data$angle <- ifelse(angle < -90, angle+180, angle)
+
+# prepare a data frame for base lines
+base_data <- dat_circular %>% 
+  group_by(region) %>% 
+  summarize(start=min(id,na.rm=T), end=max(id,na.rm=T) - empty_bar) %>% 
+  rowwise() %>% 
+  mutate(title=mean(c(start, end)))
+
+# prepare a data frame for grid (scales)
+grid_data <- base_data
+grid_data$end <- grid_data$end[ c( nrow(grid_data), 1:nrow(grid_data)-1)] + 1
+grid_data$start <- grid_data$start - 1
+grid_data <- grid_data[-1,]
+
+# Make the plot
+p <- ggplot(dat_circular) +      
+  
+  # Add the stacked bar
+  geom_bar(aes(x=as.factor(id), y=proportion, fill=food_type), 
+           position="stack",stat="identity", alpha=0.5) +
+  scale_fill_brewer(palette = "Spectral",direction=1)+
+  
+  # Add a val=100/75/50/25 lines. I do it at the beginning to make sur barplots are OVER it.
+  geom_segment(data=grid_data, aes(x = end, y = 0, xend = start, yend = 0), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=grid_data, aes(x = end, y = 15, xend = start, yend = 15), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=grid_data, aes(x = end, y = 30, xend = start, yend = 30), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=grid_data, aes(x = end, y = 45, xend = start, yend = 45), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  geom_segment(data=grid_data, aes(x = end, y = 60, xend = start, yend = 60), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+  
+  
+  # Add text showing the value of each 100/75/50/25 lines
+  ggplot2::annotate("text", x = rep(max(dat_circular$id),5), 
+                    y = c(0, 15, 30, 45, 60), 
+                    label = c("0", "15", "30", "45", "60") , 
+                    color="grey", size=3 , angle=0, fontface="bold", hjust=1) +
+  
+  ylim(-50,max(label_data$tot, na.rm=T)) +
+  theme_minimal() +
+  theme(
+    legend.position = "right",
+    legend.direction = "vertical",
+    legend.key.size = unit(0.3, 'cm'),
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    panel.grid = element_blank(),
+    plot.margin = unit(rep(-1,4), "cm") 
+  ) +
+  coord_polar() +
+  
+  # Add labels on top of each bar
+  geom_text(data=label_data, aes(x=id, y=tot+2, 
+                                 label=income_cat, hjust=hjust), 
+            color="black", fontface="bold",alpha=0.6, size=2, 
+            angle= label_data$angle, inherit.aes = FALSE ) +
+  
+  # Add base line information
+ geom_segment(data=base_data, aes(x = start, y = -5, xend = end, yend = -5), colour = "black", alpha=0.8, size=0.6 , inherit.aes = FALSE )  +
+  geom_text(data=base_data, aes(x = title, y = -18, label=region), hjust=c(1,1,0,0), colour = "black", alpha=0.8, size=3, fontface="bold", inherit.aes = FALSE)
+
+
+p
+
 # ----------------------------------------------------
 
 
@@ -177,7 +284,43 @@ states_consumption <- dplyr::left_join(BR_states,
                                        consumption_nutrients, 
                                        by = c("name_state" = "state_adj"))
 
+# br map
+map_BR <- ggplot(data = states_consumption %>%
+                   filter (is.na(mean_N_pop ) != T)) +
+  geom_sf(aes(fill=name_region),
+          colour="black",#NA 
+          size=.15) + 
+  theme_classic() +
+  theme (legend.position = "none",
+         axis.text = element_blank(),
+         axis.line = element_blank(),
+         axis.ticks = element_blank(),
+         panel.background = element_rect(fill = "transparent",
+                                         colour = NA_character_), # necessary to avoid drawing panel outline
+         panel.grid.major = element_blank(), # get rid of major grid
+         panel.grid.minor = element_blank(), # get rid of minor grid
+         plot.background = element_rect(fill = "transparent",
+                                        colour = NA_character_), # necessary to avoid drawing plot outline
+         legend.background = element_rect(fill = "transparent"),
+         legend.box.background = element_rect(fill = "transparent"),
+         legend.key = element_rect(fill = "transparent"))+
+  scale_fill_brewer(palette = "Spectral",direction=1)
 
+# arrange map and circular plot
+
+map_ccplot <- grid.arrange(p,
+             map_BR,
+             ncol=4,nrow=6,
+             layout_matrix = rbind (c(1,1,1,1),
+                                    c(1,1,1,1),
+                                    c(1,2,2,1),
+                                    c(1,2,2,1),
+                                    c(1,1,1,1),
+                                    c(1,1,1,1)))
+
+
+ggsave(map_ccplot, file=here ('output',"output.pdf"), 
+       width=10, height=10,bg="white")
 
 
 # map
@@ -215,6 +358,7 @@ total_consumption_data <- states_consumption %>%
   filter (is.na(state )!=T) #%>%  # not coastal state
   #filter (income_cat == "Class E") # class E for a while
   
+
 
 # plot
 p1 <- ggplot(data = total_consumption_data) +
